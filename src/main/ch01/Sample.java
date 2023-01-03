aa7c84bf8a8cdbb755f0fdf02ca46765dbee6965
raycho <raycho521@yonsei.ac.kr>
2023-01-03 14:44:14 +0900

    feat : add sample class and test

---
+++ src/main/ch01/Sample.java
@@ -0,0 +1,17 @@
+package ch01;
+
+import org.junit.jupiter.api.Test;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    /**
+     * cmd + shift + T : 테스트 클래스 생성
+     */
+
+    public static String sayHello(){
+        return "hello";
+    }
+}
--- src/test/ch01/SampleTest.java
+++ src/test/ch01/SampleTest.java
@@ -2,12 +2,11 @@ package ch01;
 
 import org.junit.jupiter.api.Test;
 
-import static ch01.Main.sayHello;
+import static ch01.Sample.sayHello;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class StringTest {
-
+class SampleTest {
     @Test
     void substring() {
         String str = "abcde";
@@ -19,4 +18,5 @@ public class StringTest {
     void 헬로테스트() {
         assertThat(sayHello()).isEqualTo("hello");
     }
+
 }
